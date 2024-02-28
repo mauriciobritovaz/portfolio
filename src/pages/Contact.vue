2f3af87b34ba770f2e14ede954bf51527de77a1c
Mauricio Brito Vaz <m.britovaz@gmail.com>
2024-02-28 14:01:52 -0300

    Changes in the contact page and creation of alert box

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,4 +1,63 @@
-<script></script>
+<script setup>
+import { ref } from 'vue'
+
+import alertBox from '../components/alertBox.vue';
+
+const email = ref('')
+const name = ref('')
+const message = ref('')
+const alertMessage = ref({
+  type: '',
+  title: '',
+  message: ''
+})
+const showAlert = ref(false)
+
+const submitContact = () => {
+  const validationMsg = validateForm()
+
+  if(validationMsg === 1){
+    alertMessage.value = {
+      type: 'success',
+      title: 'Success!',
+      message: `The message was sent`,
+    }
+  }
+  else if(validationMsg === 2){
+    alertMessage.value = {
+      type: 'error',
+      title: 'Sorry',
+      message: 'Please, fill all the fields',
+    }
+  }
+  else if(validationMsg === 3){
+    alertMessage.value = {
+      type: 'error',
+      title: 'Sorry',
+      message: 'The e-mail is invalid',
+    }
+  }
+
+  showAlert.value = true
+  setTimeout(() => {
+    showAlert.value = false
+  }, 3000)
+}
+
+const validateForm = () =>{
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  if(email.value.length && name.value.length && message.value.length){
+    if(emailRegex.test(email.value)){
+      return 1
+    }
+    else{
+      return 3
+    }
+  }else
+    return 2
+}
+</script>
 
 <template>
   <div class="grid grid-cols-10 text-begezito-6 font-raleway mt-20 lg:mt-20 xl:mt-10">
@@ -11,9 +70,12 @@
       </div>
     </section>
     <section class="col-span-8 col-start-2 lg:col-span-6 lg:col-start-3 xl:col-span-4 xl:col-start-4 mt-4">
-      <form class="bg-begezito-4 shadow-md p-4 rounded-md">
+      <form
+        class="bg-begezito-4 shadow-md p-4 rounded-md"
+        @submit.prevent="submitContact">
         <div class="flex flex-col mb-4">
           <input
+            v-model="name"
             id="name"
             placeholder="Name"
             type="text"
@@ -22,6 +84,7 @@
         </div>
         <div class="flex flex-col mb-4">
           <input
+            v-model="email"
             id="email"
             type="email"
             placeholder="E-mail"
@@ -30,17 +93,28 @@
         </div>
         <div class="flex flex-col mb-4">
           <textarea
+            v-model="message"
             id="msg"
             placeholder="Message"
-            class="bg-begezito-5 text-begezito-7 placeholder:text-begezito-6 placeholder:font-semibold focus:outline-none rounded-md p-2"
+            cols="50"
+            rows="5"
+            class="bg-begezito-5 text-begezito-7 placeholder:text-begezito-6 placeholder:font-semibold focus:outline-none rounded-md p-2 resize-none"
           ></textarea>
         </div>
         <div class="flex justify-end">
-          <button class="bg-begezito-5 rounded-md py-2 px-4 text-lg uppercase font-semibold focus:shadow-sm focus:shadow-begezito-6 focus:outline-none">
+          <button
+            type="submit"
+            class="bg-begezito-5 rounded-md py-2 px-4 text-lg uppercase font-semibold focus:shadow-sm focus:shadow-begezito-6 focus:outline-none">
             Submit
           </button>
         </div>
       </form>
     </section>
+    
+    <alertBox
+      v-if="showAlert"
+      :title="alertMessage.title"
+      :type="alertMessage.type"
+      :message="alertMessage.message"/>
   </div>
 </template>
